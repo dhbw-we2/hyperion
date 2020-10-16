a987b0091b733ce98ed154dd9c861d24b5a18f89
Gabriel Balzer <gbalzer96@gmail.com>
2020-10-16 15:59:52 +0200

    Pictures with Divs

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -32,6 +32,47 @@
     </q-list>
 </div>
 
+    <template>
+      <div class="q-pa-md">
+        <div class="q-col-gutter-md row items-start">
+          <div class="col-10">
+            <q-img
+              src="https://images-na.ssl-images-amazon.com/images/I/71Zy0yoxrdL._AC_SL1023_.jpg"
+              style="width: 100%"
+            >
+              <div class="absolute-bottom text-subtitle1 text-center">
+                Caption
+              </div>
+            </q-img>
+          </div>
+
+          <div class="col-10">
+            <q-img src="https://images-na.ssl-images-amazon.com/images/I/51Q7Psa2ufL._AC_.jpg">
+              <div class="absolute-top text-center">
+                Caption
+              </div>
+            </q-img>
+          </div>
+
+          <div class="col-10">
+            <q-img src="https://images-na.ssl-images-amazon.com/images/I/61pHNnq9dpL._AC_SL1200_.jpg">
+              <div class="absolute-bottom-right text-subtitle2">
+                Caption
+              </div>
+            </q-img>
+          </div>
+
+          <div class="col-10">
+            <q-img src="https://images-na.ssl-images-amazon.com/images/I/71aL85dFDIL._AC_SY550_.jpg">
+              <div class="absolute-full text-subtitle2 flex flex-center">
+                Caption
+              </div>
+            </q-img>
+          </div>
+        </div>
+      </div>
+    </template>
+
 
   </q-page>
 
