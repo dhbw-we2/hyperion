0ca44fda37d732edf39341da2145a4ea1f008f63
michaela-gui <56273662+michaela-gui@users.noreply.github.com>
2020-10-30 12:08:34 +0100

    Add login page.

---
+++ src/pages/UserLogin.vue
@@ -0,0 +1,16 @@
+<template>
+<div class = "userlogin">
+
+</div>
+</template>
+
+<script>
+export default {
+  name: "UserLogin",
+  data: function () {
+    return{movieArray: []}
+  }
+}
+</script>
+
+
