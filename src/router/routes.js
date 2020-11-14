7bca25a3b9b161d6f4b945f487460f84b5a230f6
Gabriel Balzer <gbalzer96@gmail.com>
2020-11-14 11:08:55 +0100

    Add Login and Forgot Password Page

--- src/router/routes.js
+++ src/router/routes.js
@@ -10,8 +10,16 @@ const routes = [
       { path: '/trackerlist', component: () => import('pages/trackerlist.vue') },
       { path: '/login', component: () => import('pages/UserLogin.vue') }
     ]
+  },
+  {
+    path: '/auth',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: 'login', name: 'Login', component: () => import('pages/Auth')},
+      { path: 'forgotPassword', name: 'ForgotPassword', component: () => import('pages/ForgotPassword.vue')}
+    ]
   }
-]
+  ]
 
 // Always leave this as last one
 routes.push({
