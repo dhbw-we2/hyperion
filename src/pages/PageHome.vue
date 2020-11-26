2196d7bcc6d23fbdcbecbd9c417a8668670b453e
michaela-gui <56273662+michaela-gui@users.noreply.github.com>
2020-11-26 13:18:23 +0100

    Update Homepage to click on poster.

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,58 +1,5 @@
 <template id="movieArray">
   <q-page class="bg-home">
-<!--
-    <div class="q-pa-md doc-container">
-
-      <div class="row items-start">
-
-        <div class="col">
-          Poster 1
-          <q-img @click="$router.push({ path: 'searcher' })"
-                 src="https://images-na.ssl-images-amazon.com/images/I/71Zy0yoxrdL._AC_SL1023_.jpg"
-                 class="hover rounded-borders"></q-img>
-        </div>
-
-        <div class="col">
-          Poster 2
-          <q-img src="https://images-na.ssl-images-amazon.com/images/I/51Q7Psa2ufL._AC_.jpg"
-                 class="rounded-borders"></q-img>
-        </div>
-
-        <div class="col">
-          Poster 3
-
-          <q-img src="https://images-na.ssl-images-amazon.com/images/I/61pHNnq9dpL._AC_SL1200_.jpg"
-                 class="rounded-borders"></q-img>
-        </div>
-
-        <div class="col">
-          Poster 4
-          <q-img src="https://images-na.ssl-images-amazon.com/images/I/71aL85dFDIL._AC_SY550_.jpg"
-                 class="rounded-borders"></q-img>
-        </div>
-
-        <div class="col">
-          <div class="row">
-            Poster 5
-            <q-img src="https://images-na.ssl-images-amazon.com/images/I/510hYySJ6HL._AC_.jpg"
-                   class="rounded-borders"></q-img>
-          </div>
-          <div class="row">
-            Poster 11
-            <q-img src="https://images-na.ssl-images-amazon.com/images/I/61pHNnq9dpL._AC_SL1200_.jpg"
-                   class="rounded-borders"></q-img>
-          </div>
-        </div>
-
-        <div class="col">
-          Poster 6
-          <q-img src="https://images-na.ssl-images-amazon.com/images/I/5184-DjkaVL._AC_.jpg"
-                 class="rounded-borders"></q-img>
-        </div>
-
-      </div>
-    </div>-->
-
     <q-page v-if="movieArray.title=undefined" class="flex-center">
       test
     </q-page>
@@ -63,23 +10,23 @@
         <div class="row items-start">
           <div v-for="title in movieArray">
 
-
             <q-img class="border-poster"
                    :src="'https://image.tmdb.org/t/p/w200' + title.poster_path"
                    :alt="title.title"
                    width="200px"
-                   @click="$router.push({ path: title.original_title })"
-            ></q-img>
+                   @click="handleBlur" placeholder="first name"
 
+            ></q-img>
 
           </div>
         </div>
       </div>
-
-
+      <!-- @click="this.loadData" -->
+      <!--                   @click="$router.push({ path: title.original_title })"-->
+<!--
     <div class="col" v-for="title in movieArray">
       {{ title }}
-    </div>
+    </div> -->
     </q-page>
 
   </q-page>
@@ -102,11 +49,12 @@ export default {
   },
   name: 'PageHome',
   data: function(){
-    return{
-      movieArray:[]
-    }
+      return {
+        movieArray: []
+      }
   },
   //async created() {},
+
 /*  async beforeCreate() {
     this.search = this.$route.params.search;
     this.loadData(this.search)
@@ -126,13 +74,20 @@ export default {
           this.data.results.forEach(function (entry) {
           })
           this.movieArray = this.data.results
-          console.log("test function")
           return this.data.results
 
         })
         .catch(() => {
-          console.log("test catch")
+
         })
+    },
+
+    handleBlur(event) {
+      const el = event.target
+      // do something here
+      //console.log('focus was set here')
+      let index = this.movieArray.title 
+      console.log(index)
     }
   }
 
